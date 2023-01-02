e6814c5fc6c7d2f8b3c85ed99c74cb53d216f9da
Luke Henry <luke.henry@flowstatesolutions.ai>
2023-01-02 10:38:33 -0700

    Adding scss

--- src/main.js
+++ src/main.js
@@ -3,6 +3,21 @@ import vuetify from '@/plugins/vuetify'
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+
+import i18n from './plugins/vue-i18n'
+import './plugins/vue-google-maps'
+import './plugins/vue-shortkey'
+import './plugins/vue-head'
+import './plugins/vue-gtag'
+import './plugins/apexcharts'
+import './plugins/echarts'
+import './plugins/animate'
+import './plugins/clipboard'
+import './plugins/moment'
+
+import './assets/scss/theme.scss'
+
+import 'animate.css/animate.min.css'
 // import { makeServer } from "./server";
 // import VueEasyLightbox from 'vue-easy-lightbox'
 
@@ -16,6 +31,7 @@ Vue.config.productionTip = false;
 
 new Vue({
 	// VueEasyLightbox,
+	i18n,
 	router,
 	store,
 	vuetify,
